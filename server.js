aa85d30e138f6a0da3cc3ef1e8a9c53d0cafeae0
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-08-27 20:03:16 -0500

    moved the * route to before the /notes route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const api = require("public/assets/js/index.js");
+const api = require("./routes/import");
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,12 +12,12 @@ app.use("/api", api);
 
 app.use(express.static("public"));
 
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/notes.html"))
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
 app.listen(PORT, () =>
